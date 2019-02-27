b2a36cf02b29eb01fe5679ac34f264123ed3dd5b
Alexey Turkin <altsworks@gmail.com>
2019-02-27 11:43:36 +0300

    app_ui: setup toolbar

--- app/src/main/java/com/bytebuilding/memento/ui/add/AddFactActivity.kt
+++ app/src/main/java/com/bytebuilding/memento/ui/add/AddFactActivity.kt
@@ -3,6 +3,8 @@ package com.bytebuilding.memento.ui.add
 import androidx.databinding.ViewDataBinding
 import com.bytebuilding.memento.R
 import com.bytebuilding.memento.ui.base.BaseActivity
+import com.bytebuilding.memento.utils.setUpToolbar
+import kotlinx.android.synthetic.main.toolbar_base.*
 
 
 class AddFactActivity :
@@ -11,6 +13,10 @@ class AddFactActivity :
     override fun layoutId(): Int = R.layout.activity_add_fact
 
     override fun initViews() {
+        setUpToolbar(
+            toolbar = toolbar,
+            title = R.string.add_fact_screen_title
+        )
     }
 
     override fun initListeners() {
