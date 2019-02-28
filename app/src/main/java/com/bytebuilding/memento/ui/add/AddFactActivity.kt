ad31c921a4ddf6a9d2554123b9c913bab700650a
Alexey Turkin <altsworks@gmail.com>
2019-02-28 16:22:07 +0300

    app_ui: implemented onTextChanged logic in terms of ViewState logic

--- app/src/main/java/com/bytebuilding/memento/ui/add/AddFactActivity.kt
+++ app/src/main/java/com/bytebuilding/memento/ui/add/AddFactActivity.kt
@@ -3,14 +3,19 @@ package com.bytebuilding.memento.ui.add
 import android.view.Menu
 import android.view.MenuItem
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Observer
 import com.bytebuilding.memento.R
 import com.bytebuilding.memento.ui.base.BaseActivity
+import com.bytebuilding.memento.ui.custom.AddInformationView
 import com.bytebuilding.memento.utils.setUpToolbar
 import com.bytebuilding.memento.utils.shortToast
+import kotlinx.android.synthetic.main.activity_add_fact.*
 import kotlinx.android.synthetic.main.toolbar_base.*
 
 class AddFactActivity :
     BaseActivity<ViewDataBinding, AddFactActivityVM, AddFactActivityVM.ViewState>(AddFactActivityVM::class) {
+
+    private var mSaveMenuItem: MenuItem? = null
 
     override fun layoutId(): Int = R.layout.activity_add_fact
 
@@ -24,27 +29,45 @@ class AddFactActivity :
     }
 
     override fun initListeners() {
+        addTitle.setOnInformationTextChangedListener(object : AddInformationView.InformationTextChangedListener {
+            override fun onTextChanged(text: CharSequence?) {
+                mViewModel.onTitleChanged(text.toString())
+            }
+        })
+
+        addDescription.setOnInformationTextChangedListener(object : AddInformationView.InformationTextChangedListener {
+            override fun onTextChanged(text: CharSequence?) {
+                mViewModel.onDescriptionChanged(text)
+            }
+        })
     }
 
     override fun observeChanges() {
+        mViewModel.mViewState.observe(this, Observer {
+            it?.let { viewState -> render(viewState) }
+        })
     }
 
     override fun removeListeners() {
+        addTitle.removeInformationTextChangedListener()
+        addDescription.removeInformationTextChangedListener()
     }
 
     override fun render(viewState: AddFactActivityVM.ViewState) {
         when (viewState.isTitleValid && viewState.isDescriptionValid) {
             true -> {
-                //Enable save menu
+                mSaveMenuItem?.isEnabled = true
             }
             false -> {
-                //Disable save menu
+                mSaveMenuItem?.isEnabled = false
             }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.add_fact_menu, menu)
+        mSaveMenuItem = menu?.findItem(R.id.actionSave)
+        mSaveMenuItem?.isEnabled = false
         return true
     }
 
@@ -56,5 +79,9 @@ class AddFactActivity :
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object {
+        const val TAG = "AddFactActivity"
     }
 }
