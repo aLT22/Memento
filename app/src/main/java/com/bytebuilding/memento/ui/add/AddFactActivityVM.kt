a760292ec4303b8bc91f90d975a71aff70774858
Alexey Turkin <altsworks@gmail.com>
2019-02-28 12:56:01 +0300

    app_ui: added basic version of view state in VM

--- app/src/main/java/com/bytebuilding/memento/ui/add/AddFactActivityVM.kt
+++ app/src/main/java/com/bytebuilding/memento/ui/add/AddFactActivityVM.kt
@@ -1,7 +1,36 @@
 package com.bytebuilding.memento.ui.add
 
+import androidx.lifecycle.MutableLiveData
 import com.bytebuilding.memento.ui.base.BaseViewModel
 
 
 class AddFactActivityVM : BaseViewModel() {
+
+    /**
+     * Activity's view state
+     * */
+    data class ViewState(
+        val title: String = "",
+        val isTitleValid: Boolean = false,
+        val description: String = "",
+        val isDescriptionValid: Boolean = false
+    )
+
+    val viewState = MutableLiveData<ViewState>()
+
+    init {
+        viewState.value = ViewState()
+    }
+
+    fun onTitleChanged(title: String) {
+        viewState.value =
+            if (title.isEmpty()) {
+                currentViewState().copy(title = title, isTitleValid = false)
+            } else {
+                currentViewState().copy(title = title, isTitleValid = true)
+            }
+    }
+
+    private fun currentViewState(): ViewState = viewState.value!!
+
 }
