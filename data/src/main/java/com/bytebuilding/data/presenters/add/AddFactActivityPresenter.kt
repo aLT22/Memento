cacc83c8e9aefe71cff8fd10f610be0e551884d6
Alexey Turkin <altsworks@gmail.com>
2019-03-11 22:37:17 +0300

    data: added reactive presenter for AddFactActivity

---
+++ data/src/main/java/com/bytebuilding/data/presenters/add/AddFactActivityPresenter.kt
@@ -0,0 +1,58 @@
+package com.bytebuilding.data.presenters.add
+
+import com.bytebuilding.data.presenters.base.BasePresenter
+import com.bytebuilding.data.utils.loge
+import com.bytebuilding.domain.messages.add.AddFactActivityActions
+import com.bytebuilding.domain.messages.add.AddFactActivityEvents
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlin.coroutines.CoroutineContext
+
+
+const val TAG = "AddFactPresenter"
+
+data class AddFactActionProducer(
+    val addFactActionChannel: ReceiveChannel<AddFactActivityActions>
+)
+
+class AddFactActivityPresenter {
+
+    companion object Presenter : BasePresenter, CoroutineScope {
+
+        private val mJob = SupervisorJob()
+        override val coroutineContext: CoroutineContext
+            get() = Dispatchers.IO + mJob
+
+        fun addFactActivityEventCatcher(
+            addFactActivityEventChannel: ReceiveChannel<AddFactActivityEvents>
+        ): AddFactActionProducer {
+            val addFactActivityActionChannel = Channel<AddFactActivityActions>()
+
+            launch {
+                try {
+                    for (addFactActivityEvent in addFactActivityEventChannel) {
+                        when (addFactActivityEvent) {
+                            AddFactActivityEvents.FactWasAddedEvent -> {
+                                //TODO: implementation logic after adding fact
+                                false
+                            }
+                        }
+                    }
+                } catch (th: Throwable) {
+                    th.printStackTrace()
+                    loge(TAG, th.localizedMessage, th)
+                }
+            }
+
+            return AddFactActionProducer(addFactActivityActionChannel)
+
+        }
+
+        override fun cancelJobs() {
+            mJob.cancelChildren()
+        }
+
+    }
+
+}
