b36bbecf4cef18ea707646a652ebb09bdadf9645
Alexey Turkin <altsworks@gmail.com>
2019-03-11 22:38:03 +0300

    data: implemented data source and repository; code refactoring

---
+++ data/src/main/java/com/bytebuilding/data/repositories/fact/FactRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.bytebuilding.data.repositories.fact
+
+import com.bytebuilding.domain.model.Fact
+import com.bytebuilding.domain.repositories.fact.FactDataSource
+import com.bytebuilding.domain.repositories.fact.FactRepository
+
+
+class FactRepositoryImpl(
+    private val mLocalDataSource: FactDataSource
+) : FactRepository {
+
+    override fun getAllFacts(): List<Fact> = mLocalDataSource.getAllFacts()
+
+    override fun getFactById(id: Long): Fact = mLocalDataSource.getFactById(id)
+
+    override fun getFactsByTitle(title: CharSequence): List<Fact> = mLocalDataSource.getFactsByTitle(title)
+
+    override fun getFactsByDescription(description: CharSequence): List<Fact> =
+        mLocalDataSource.getFactsByDescription(description)
+}
