8d92cd49fc6c8d92495279c10ab7f9da6741252c
Alexey Turkin <altsworks@gmail.com>
2019-03-12 08:23:54 +0300

    domain: code refactoring

--- domain/src/main/java/com/bytebuilding/domain/messages/add/AddFactActivityEvents.kt
+++ domain/src/main/java/com/bytebuilding/domain/messages/add/AddFactActivityEvents.kt
@@ -1,8 +1,12 @@
 package com.bytebuilding.domain.messages.add
 
+import com.bytebuilding.domain.model.Fact
+
 
 sealed class AddFactActivityEvents {
 
-    object FactWasAddedEvent : AddFactActivityEvents()
+    object FactWasAddedEvent : AddFactActivityEvents() {
+        var fact: Fact? = null
+    }
 
 }
