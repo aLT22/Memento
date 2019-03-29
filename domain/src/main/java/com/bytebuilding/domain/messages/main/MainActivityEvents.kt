07dff18be74d25826b3a42bab495866c44db3b71
Alexey Turkin <altsworks@gmail.com>
2019-03-30 01:52:41 +0300

    domain_messages: added new messages for deleting facts

--- domain/src/main/java/com/bytebuilding/domain/messages/main/MainActivityEvents.kt
+++ domain/src/main/java/com/bytebuilding/domain/messages/main/MainActivityEvents.kt
@@ -1,10 +1,35 @@
 package com.bytebuilding.domain.messages.main
 
+import com.bytebuilding.domain.model.Fact
+
 
 sealed class MainActivityEvents {
 
+    /**
+     * Add fact events
+     * */
     object AddFactEvent : MainActivityEvents()
+    /**
+     * Add fact events
+     * */
 
+    /**
+     * Retrieve facts events
+     * */
     object RetreiveFactsEvent : MainActivityEvents()
+    /**
+     * Retrieve facts events
+     * */
+
+    /**
+     * Delete fact events
+     * */
+    object DeleteFactEvent : MainActivityEvents() {
+        var mIndex: Int? = null
+        var mFact: Fact? = null
+    }
+    /**
+     * Delete fact events
+     * */
 
 }
